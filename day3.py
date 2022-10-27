499fbb0c4ae6467d9848e75a3777665bf5c7521c
Keosha Young <kyoung1012@gmail.com>
2022-10-27 19:18:41 -0400

    Add files via upload

---
+++ day3.py
@@ -0,0 +1,7 @@
+food = input("Name a food")
+plant = input("Name a plant")
+cooking = input("Describe a cooking method")
+burnt = input("Describe burned food")
+item = input("Name a household item")
+print("Oh my! You guys are", cooking, food, "with",
+      burnt, plant, "on a bed of", item, "!")
